0c85b366dd47b1371e23d37e6e046b145fd1047f
aju-alen <alenaju1304@gmail.com>
2025-03-29 19:13:30 +0400

    footer color

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="bg-trump-light-primary text-trump-light-navy pt-16 pb-6">
+    <footer className="bg-trump-maingreen text-trump-light-navy pt-16 pb-6">
       <div className="container mx-auto px-4">
         <div className="mb-14">
           <h3 className="text-lg font-bold mb-4">DOWNLOAD THE FUNYULA 2027
@@ -26,7 +26,7 @@ const Footer = () => {
           <SignupForm variant="footer" />
         </div>
 
-        <div className="text-xs text-gray-600 border-t border-gray-200 pt-6 flex flex-col sm:flex-row justify-between items-center text-center sm:text-left px-4 sm:px-8 lg:px-16">
+        <div className="text-xs text-white border-t border-gray-200 pt-6 flex flex-col sm:flex-row justify-between items-center text-center sm:text-left px-4 sm:px-8 lg:px-16">
   {/* Left Section */}
   <div className="flex flex-col gap-2 max-w-md">
     {/* <p className="mb-3">
@@ -38,7 +38,7 @@ const Footer = () => {
   </div>
 
   {/* Footer Nav Bar */}
-  <div className="flex flex-col sm:flex-row flex-wrap gap-4 text-gray-600 justify-center sm:justify-end items-center">
+  <div className="flex flex-col sm:flex-row flex-wrap gap-4 text-white justify-center sm:justify-end items-center">
     <Link to="/news" className="hover:text-trump-light-accent transition-colors">PRIVACY POLICY</Link>
     <Link to="/platform" className="hover:text-trump-light-accent transition-colors">TERMS & CONDITION</Link>
     <Link to="/upcoming-projects" className="hover:text-trump-light-accent transition-colors">UPCOMING PROJECTS</Link>
