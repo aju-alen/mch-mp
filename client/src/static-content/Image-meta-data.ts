73b6ba5c85bdbf17f9c8eb6fac5d82eae6fdb310
aju-alen <alenaju1304@gmail.com>
2025-07-17 13:17:08 +0400

    image url fix

--- client/src/static-content/Image-meta-data.ts
+++ client/src/static-content/Image-meta-data.ts
@@ -154,193 +154,193 @@ export const videos: Video[] = [
       id: '25',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250410_182509000_iOS+1.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250410_182509000_iOS+1.jpg'
     },
      {
       id: '26',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250410_182511000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250410_182511000_iOS.jpg'
     },
     {
       id: '27',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_004713000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_004713000_iOS.jpg'
     },
     {
       id: '28',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_004808000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_004808000_iOS.jpg'
     },
     {
       id: '29',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005005000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005005000_iOS.jpg'
     },
     {
       id: '30',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005030000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005030000_iOS.jpg'
     },
     {
       id: '31',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005031000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005031000_iOS.jpg'
     },
     {
       id: '32',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005232000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005232000_iOS.jpg'
     },
     {
       id: '33',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005239000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005239000_iOS.jpg'
     },
     {
       id: '34',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005243000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005243000_iOS.jpg'
     },
     {
       id: '35',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005328000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005328000_iOS.jpg'
     },
     {
       id: '36',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005334000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005334000_iOS.jpg'
     },
     {
       id: '37',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005337000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005337000_iOS.jpg'
     },
     {
       id: '38',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005337000_iOS1.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005337000_iOS1.jpg'
     },
     {
       id: '39',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005832000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005832000_iOS.jpg'
     },
     {
       id: '40',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005843000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005843000_iOS.jpg'
     },
     {
       id: '41',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005844000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005844000_iOS.jpg'
     },
     {
       id: '42',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_005847000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_005847000_iOS.jpg'
     },
     {
       id: '43',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_013911000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_013911000_iOS.jpg'
     },
     {
       id: '44',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_013911000_iOS1.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_013911000_iOS1.jpg'
     },
     {
       id: '45',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145455000_iOS1.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145455000_iOS1.jpg'
     },
     {
       id: '46',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145500000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145500000_iOS.jpg'
     },
     {
       id: '47',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145501000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145501000_iOS.jpg'
     },
     {
       id: '48',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145502000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145502000_iOS.jpg'
     },
     {
       id: '49',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145509000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145509000_iOS.jpg'
     },
     {
       id: '50',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145511000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145511000_iOS.jpg'
     },
     {
       id: '51',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145511000_iOS1.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145511000_iOS1.jpg'
     },
     {
       id: '52',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145512000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145512000_iOS.jpg'
     },
     {
       id: '53',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250416_145519000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250416_145519000_iOS.jpg'
     },
     {
       id: '54',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250420_081646000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250420_081646000_iOS.jpg'
     },
     {
       id: '55',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250506_114405000_iOS+2.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250506_114405000_iOS+2.jpg'
     },
     {
       id: '56',
       title: 'Title Here',
       description: 'Description of what he is doing or what he promises here',
-      imageUrl: 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/funyula-images/20250506_144509000_iOS.jpg'
+      imageUrl: 'https://d177n02wywg7h2.cloudfront.net/funyula-images/20250506_144509000_iOS.jpg'
     },
     
   ];
