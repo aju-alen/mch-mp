9b35833b48a7c42cf7efd95f38eb43fb5a818574
aju-alen <alenaju1304@gmail.com>
2025-07-18 12:20:47 +0400

    news change

--- client/src/utils/newsData.ts
+++ client/src/utils/newsData.ts
@@ -28,7 +28,7 @@
 
       <p style=" font-size:1.25rem;">Hon. Mugenya assured the women that their concerns would be a top priority in his development agenda. “For trade to flourish in Funyula, we must create an enabling environment. Reliable electricity will not only improve security but also open opportunities for modern business practices. Clean water is equally critical for health and sanitation, which our women traders require,” he said.</p>
 
-      <p style=" font-size:1.25rem;">Hon. Mugenya emphasised that improved infrastructure would spur economic growth, empower women traders, and enhance livelihoods across the constituency. “Investing in market utilities is an investment in our people, and the current government sees the need for this, with over 400+ modern market projects on-going,” Mugenya added.</p>
+      <p style=" font-size:1.25rem;">Hon. Mugenya emphasised that improved infrastructure would spur economic growth, empower women traders, and enhance livelihoods across the constituency. “Investing in market utilities is an investment in our people, and the current government sees the need for this, with over 400+ modern market projects on-going,” he added.</p>
 
       <p style=" font-size:1.25rem;">The meeting concluded with a shared commitment to work collaboratively towards these goals. The women pledged to support initiatives that will bring sustainable change to the community.</p>
 
