294be8a60350b66559495e0ade74ecc8c21e94f1
aju-alen <alenaju1304@gmail.com>
2025-07-18 11:54:45 +0400

    changes on data

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="container relative z-10 
         md:absolute 
         md:bottom-30 
-        md:right-20
+        md:right-[85px]
         px-4 
         xl:px-4 
         2xl:px-0
