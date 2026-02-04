36e578e04a0204d044cdfa4375f20db1155f6abe
aju-alen <alenaju1304@gmail.com>
2026-02-04 13:46:53 +0400

    build vercel

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from 'next'
- 
-const nextConfig: NextConfig = {
-  distDir: 'build', // Changes the build output directory to `build`
-}
- 
+
+const nextConfig: NextConfig = {}
+
 export default nextConfig
