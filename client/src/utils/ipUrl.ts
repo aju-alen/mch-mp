709e466c94a41c08a73159df453496514b8a9ad9
aju-alen <alenaju1304@gmail.com>
2026-02-04 14:55:13 +0400

    url

--- client/src/utils/ipUrl.ts
+++ client/src/utils/ipUrl.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001'
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://future.funyula.com'
 
 /** Use this URL when sharing the contribute page so WhatsApp/Google show the correct preview */
 export const CONTRIBUTE_SHARE_URL =
